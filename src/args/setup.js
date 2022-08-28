aed224f6da025dcf148b185124e6647e1456a612
Tesura <AnzenKodo@altmails.com>
2022-08-29 02:06:42 +0530

    Added .gitignore file in setup

--- src/args/setup.js
+++ src/args/setup.js
@@ -27,9 +27,11 @@ Bring to the table win-win survival strategies to ensure proactive domination. A
 
 Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.`;
 
+const gitignore = `site/`;
 export default function setup() {
   ensureDirSync("./posts/");
   ensureDirSync("./.github/workflows");
+  Deno.writeTextFileSync("./.gitignore", gitignore);
   Deno.writeTextFileSync("./config.json", JSON.stringify(data, null, 2));
   Deno.writeTextFileSync("./posts/dblog-Docs.md", dblogDocs);
   Deno.writeTextFileSync("./posts/_template.md", template);
